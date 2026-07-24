30cbf16f15a4acef1559bc8b9e1d4b5666550d5f
Aishwarya M <231801003@rajalakshmi.edu.in>
2026-07-24 11:21:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-app = Flask(_name_)
+app = Flask(__name__)
 @app.route("/")
 def home():
  return "Hello! Welcome to Render PaaS."
 @app.route("/about")
 def about():
  return "This application is deployed on Render."
-if _name_ == "_main_":
+if __name__ == "__main__":
  app.run(host="0.0.0.0", port=5000)
